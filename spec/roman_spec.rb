2277d2128653b3ea1d6e6e18bcbc5f0122141e25
Joseph W Galvin <jwgalvin@gmail.com>
2022-06-07 16:24:02 -0600

    clean up specs

--- spec/roman_spec.rb
+++ spec/roman_spec.rb
@@ -7,28 +7,28 @@ RSpec.describe " dumb stuff" do
   it "test to see how far off I am" do
     int = 428
     roman = RomanNumeralConverter.new
-    #binding.pry
+
     expect(roman.to_roman(int)).to eq("CDXXVIII")
   end
 
   it "test to see how far off I am" do
     int = 3999
     roman = RomanNumeralConverter.new
-    #binding.pry
+
     expect(roman.to_roman(int)).to eq("MMMCMXCIX")
   end
 
   it "test to see how far off I am" do
     int = 666
     roman = RomanNumeralConverter.new
-    #binding.pry
+
     expect(roman.to_roman(int)).to eq("DCLXVI")
   end
 
   it "test to see how far off I am" do
     int = 69
     roman = RomanNumeralConverter.new
-    #binding.pry
+
     expect(roman.to_roman(int)).to eq("LXIX")
   end
 end
