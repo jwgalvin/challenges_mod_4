99a5d481ac21b9a56dce070ef948199f507723b0
Joseph W Galvin <jwgalvin@gmail.com>
2022-06-07 19:39:01 -0600

    possible solution

--- lib/robot_path.rb
+++ lib/robot_path.rb
@@ -11,7 +11,7 @@
 
 #Assuming North is the starting direction for all directions
 #assume 2 dimensional movement on X,Y Axis, and only 'L', 'R', or 'G' are valid instructions.
-# Return "is a circle" or "is not circular"
+# Return "returned home" or "lost in the gloom"
 class love_death_robots
 
   def initialize
@@ -21,15 +21,59 @@ class love_death_robots
   end
 
   def return_to_origin(orders)
-    commands = orders.chars.capitalize()
+    commands = orders.chars.capitalize()  #cap may not be necessary
     temp_x = @init_X
     temp_y = @init_Y
     direction = @direction[0].keys
-      commands.each do |order|
+    count = 0
+    while commands.count > 0
+      case orders
+      when commands[count] == "G"
         if order == "G" && direction == "N"
           temp_y += 1
+          commands = commands.shift
         elsif order == "G" && direction == "E"
-
+          temp_x += 1
+          commands = commands.shift
+        elsif order == "G" && direction == "S"
+          temp_y -= 1
+          commands = commands.shift
+        else order == "G" && direction == "W"
+          temp_x -= 1
+          commands = commands.shift
+        end
+      end
+      when commands[count] == "L"
+        if direction == "N"
+          direction = "W"
+          commands = commands.shift
+        elsif direction == "W"
+          direction = "S"
+          commands = commands.shift
+        elsif direction == "S"
+          direction = "E"
+          commands = commands.shift
+        else
+          direction = "N"
+          commands = commands.shift
+        end
+      end
+      when commands[count] == "R"
+        if direction == "N"
+          direction = "E"
+          commands = commands.shift
+        elsif direction == "E"
+          direction = "S"
+          commands = commands.shift
+        elsif direction == "S"
+          direction = "W"
+          commands = commands.shift
+        else
+          direction = "N"
+          commands = commands.shift
+        end
+      end
+    end
   end
 
 end
