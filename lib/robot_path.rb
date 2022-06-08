3e3d17f834d03d494dc342967094c4a495ead14e
Joseph W Galvin <jwgalvin@gmail.com>
2022-06-07 21:22:00 -0600

    works

--- lib/robot_path.rb
+++ lib/robot_path.rb
@@ -12,6 +12,8 @@
 #Assuming North is the starting direction for all directions
 #assume 2 dimensional movement on X,Y Axis, and only 'L', 'R', or 'G' are valid instructions.
 # Return "returned home" or "lost in the gloom"
+require 'pry'
+
 class LoveDeathNRobots
 
   def initialize
@@ -20,65 +22,62 @@ class LoveDeathNRobots
   end
 
   def return_to_origin(orders)
-    commands = orders.chars.capitalize()  #cap may not be necessary
+    commands = orders.chars  #cap may not be necessary
     x = @init_X
     y = @init_Y
     results = ["Robot returned home", "Robot died in the wastes"]
+    @direction = "N"
     while commands.count > 0
-      case orders
-      when commands[0] == "G"
-        if order == "G" && direction == "N"
-          y += 1
-          commands = commands.shift
-        elsif order == "G" && direction == "E"
-          x += 1
-          commands = commands.shift
-        elsif order == "G" && direction == "S"
-          y -= 1
-          commands = commands.shift
-        else order == "G" && direction == "W"
-          x -= 1
-          commands = commands.shift
-        end
-      end
+      #binding.pry
 
-      when commands[0] == "L"
-        if direction == "N"
-          direction = "W"
-          commands = commands.shift
-        elsif direction == "W"
-          direction = "S"
-          commands = commands.shift
-        elsif direction == "S"
-          direction = "E"
-          commands = commands.shift
-        else
-          direction = "N"
-          commands = commands.shift
-        end
-      end
+    if commands[0] == "R"
+      if commands[0] == "R" && @direction == "N"
+        @direction = "E"
+        #commands.shift
+        #binding.pry
+      elsif commands[0] == "R" && @direction == "E"
+        @direction = "S"
+        #commands.shift
+        #binding.pry
+      elsif commands[0] == "R" && @direction == "S"
+        @direction = "W"
+        #commands.shift
+        #binding.pry
+      else commands[0] == "R" && @direction == "W"
+        @direction = "N"
 
-      when commands[0] == "R"
-        if direction == "N"
-          direction = "E"
-          commands = commands.shift
-        elsif direction == "E"
-          direction = "S"
-          commands = commands.shift
-        elsif direction == "S"
-          direction = "W"
-          commands = commands.shift
-        else
-          direction = "N"
-          commands = commands.shift
-        end
       end
-
-      if x == 0 && y == 0
-        puts results[0]
-        return results[0]
-      else
-        puts results[1]
-        return results[1]
+      commands.shift
+    elsif commands[0] == "L"
+      if commands[0] == "L" && @direction == "N"
+        @direction = "W"
+      elsif commands[0] == "L" && @direction == "W"
+        @direction = "S"
+      elsif commands[0] == "L" && @direction == "S"
+        @direction = "E"
+      else commands[0] == "L" && @direction == "E"
+        @direction = "N"
       end
+      commands.shift
+    elsif commands[0] == "G"
+      if commands[0] == "G" && @direction == "N"
+        y += 1
+      elsif commands[0] == "G" && @direction == "E"
+        x += 1
+      elsif commands[0] == "G" && @direction == "S"
+        y -= 1
+      elsif commands[0] == "G" && @direction == "W"
+        x -= 1
+      end
+      commands.shift
+    end
+  end
+    if x == 0 && y == 0
+      puts results[0]
+      return results[0]
+    else
+      puts results[1]
+      return results[1]
+    end
+end
 end
--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -13,4 +13,17 @@ RSpec.describe " dumb stuff" do
     expect(robot.return_to_origin(directions)).to eq("Robot returned home")
   end
 
+  it "test to see how far off I am" do
+    directions = "GLGLGLG"
+    robot = LoveDeathNRobots.new
+
+    expect(robot.return_to_origin(directions)).to eq("Robot returned home")
+  end
+
+  it "test to see how far off I am" do
+    directions = "GLGRGRG"
+    robot = LoveDeathNRobots.new
+
+    expect(robot.return_to_origin(directions)).to eq("Robot died in the wastes")
+  end
 end
