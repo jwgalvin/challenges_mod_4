b759dadbf3c2c6fd4d417de00c8db7b0dfc30f12
Joseph W Galvin <jwgalvin@gmail.com>
2022-06-14 08:34:49 -0600

    add instructions and file for bracket_matcher

---
+++ lib/bracket_matcher.rb
@@ -0,0 +1,20 @@
+# Instructions
+# Create a method/function that will intake a set of brackets [ { ( as a string and determine if the brackets are well-formed (match). Brackets can be nested.
+#
+#     bracket('{}')
+#     // => true
+#
+#     bracket('{()}')
+#     // => true
+#
+#     bracket('({[]}{[]})')
+#     // => true
+#
+#     bracket('{(')
+#     // => false
+#
+#     bracket('{[)][]}')
+#     // => false
+#
+#     bracket(']')
+#     // => false
