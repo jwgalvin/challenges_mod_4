d9aac45e7456f57587efdd4ea8337932493ce8c3
Joseph W Galvin <jwgalvin@gmail.com>
2022-06-14 08:35:49 -0600

    add instructions and file for snail

---
+++ lib/snail.rb
@@ -0,0 +1,15 @@
+# Instructions
+# Given an n x n array, write a method that returns the array elements arranged from outermost elements to the middle element, traveling clockwise.
+#
+# A good way to visualize this is to picture the spiral shell of a snail!
+#
+# Example
+#
+#     #Ruby
+#     array_matrix = [
+#         [1, 2, 3],
+#         [4, 5, 6],
+#         [7, 8, 9]
+#     ];
+#     snail(array_matrix)
+#     => [1, 2, 3, 6, 9, 8, 7, 4, 5]
