0004e83f33f09391b8265a01ed5e8816eef9aefa
Joseph W Galvin <jwgalvin@gmail.com>
2022-06-14 08:35:36 -0600

    add instructions and file for pascals triangle

---
+++ lib/pascals_triangle.rb
@@ -0,0 +1,8 @@
+# Instructions
+# If you’ve never heard of Pascal’s Triangle, you are not alone! Use this resource to help gain some context.
+#
+# Your goal is to write a method/function that, given a depth (n), returns an array representing Pascal’s Triangle to the n-th level.
+#
+#    #Ruby
+#    calculate_pascals_triangle(4)
+#    =>  [1, 1, 1, 1, 2, 1, 1, 3, 3, 1]
