546a4ef59ffe8b78cbf989b2c50ded8426aad82a
Joseph W Galvin <jwgalvin@gmail.com>
2022-06-20 14:07:44 -0600

    tests for bracket matcher added.

---
+++ spec/brackets_match_spec.rb
@@ -0,0 +1,38 @@
+require 'rspec'
+require 'pry'
+require './lib/bracket_matcher'
+
+RSpec.describe " checks challenges" do
+  it "see if I get the expected array" do
+    string = '{}'
+    check = PairedBrackets.new
+    expect(check.matched_set?(string)).to eq(true)
+  end
+  it "see if I get the expected array" do
+    string = '{()}'
+    check = PairedBrackets.new
+    expect(check.matched_set?(string)).to eq(true)
+  end
+  it "see if I get the expected array" do
+    string = '({[]}{[]})'
+    check = PairedBrackets.new
+    expect(check.matched_set?(string)).to eq(true)
+  end
+
+  it "see if I get the expected array" do
+    string = '{('
+    check = PairedBrackets.new
+    expect(check.matched_set?(string)).to eq(false)
+  end
+  it "see if I get the expected array" do
+    string = '{[)][]}'
+    check = PairedBrackets.new
+    expect(check.matched_set?(string)).to eq(false)
+  end
+  it "see if I get the expected array" do
+    string = ']'
+    check = PairedBrackets.new
+    expect(check.matched_set?(string)).to eq(false)
+  end
+
+end
