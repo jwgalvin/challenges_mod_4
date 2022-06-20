1b1ea5d07f20d9a7c7bdf8f3fda4f38f54dfd099
Joseph W Galvin <jwgalvin@gmail.com>
2022-06-20 13:46:10 -0600

    remove pry's

--- spec/powers_2_spec.rb
+++ spec/powers_2_spec.rb
@@ -6,7 +6,17 @@ RSpec.describe " dumb stuff" do
   it "see if I get the expected array" do
     n = 6
     power = MagicPowers.new
-    binding.pry
-    expect(power.powers_of_2(n)).to eq([1,2,4,8,16,32])
+    expect(power.powers_case(n)).to eq([1,2,4,8,16,32])
+  end
+
+  it "see if I get the expected array" do
+    n = 0
+    power = MagicPowers.new
+    expect(power.powers_case(n)).to eq([1])
+  end
+  it "see if I get the expected array" do
+    n = 1
+    power = MagicPowers.new
+    expect(power.powers_case(n)).to eq([1, 2])
   end
 end
