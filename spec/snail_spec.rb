308aa8a6bb9d8c5d3b675ad637d5776b1d2e4e14
Joseph W Galvin <jwgalvin@gmail.com>
2022-06-21 21:43:43 -0600

    snail tests added.

---
+++ spec/snail_spec.rb
@@ -0,0 +1,13 @@
+require 'rspec'
+require 'pry'
+require './lib/snail'
+
+RSpec.describe " checks challenges" do
+  it "sorts the array logarthimatically?" do
+    array = [[1, 2, 3],
+             [4, 5, 6],
+             [7, 8, 9]]
+    snail = SnailSort.new
+    expect(snail.snail_sort(array)).to eq([1, 2, 3, 6, 9, 8, 7, 4, 5])
+  end
+end
