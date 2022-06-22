f9804ba1fbdf66a6f395fe08d9e58609787686f5
Joseph W Galvin <jwgalvin@gmail.com>
2022-06-21 20:50:49 -0600

    3rd test to make sure it doesn't kill all 9's

--- spec/7ate9_spec.rb
+++ spec/7ate9_spec.rb
@@ -7,13 +7,18 @@ RSpec.describe " checks challenges" do
     string = "79712312"
     hungry = HungrySevens.new
 
-    expect(hungry(string)).to eq("7712312")
+    expect(hungry.seven_ate9(string)).to eq("7712312")
   end
 
   it "remove the 9's that are surrounded by 7's" do
     string = "79797"
     hungry = HungrySevens.new
+    expect(hungry.seven_ate9(string)).to eq("777")
+  end
 
-    expect(hungry(string)).to eq("777")
+  it "remove the 9's that are surrounded by 7's" do
+    string = "799797"
+    hungry = HungrySevens.new
+    expect(hungry.seven_ate9(string)).to eq("79977")
   end
 end
