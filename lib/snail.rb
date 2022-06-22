836d0bda72f0cfda4102becb6fdd41e8fc7343b2
Joseph W Galvin <jwgalvin@gmail.com>
2022-06-21 21:43:53 -0600

    snail kata done

--- lib/snail.rb
+++ lib/snail.rb
@@ -13,3 +13,24 @@
 #     ];
 #     snail(array_matrix)
 #     => [1, 2, 3, 6, 9, 8, 7, 4, 5]
+
+#take an array of arrays of integers and sort them into a new array organized like a snail shell. at this time this will not be variable beyond a 3 x 3 shell
+require 'pry'
+
+class SnailSort
+  def initialize
+    @straight_snail = []
+  end
+
+  def snail_sort(array)
+    if array.to_a.empty?
+      snail_secrets = @straight_snail.flatten
+      @straight_snail = []
+      return snail_secrets
+    else
+      @straight_snail << array.shift
+    end
+    snail_sort(array.transpose.reverse)
+  end
+
+end
