7bdb633032ee1b79b614e0b2476c611770c65089
Joseph W Galvin <jwgalvin@gmail.com>
2022-06-21 20:51:01 -0600

    solution variant

--- lib/7ate9.rb
+++ lib/7ate9.rb
@@ -1,4 +1,24 @@
-Write a function that removes every lone 9 that is inbetween 7s.
+# Write a function that removes every lone 9 that is inbetween 7s.
+#
+# "79712312" --> "7712312"
+# "79797"    --> "777"
+# if a string has a 9 between 7's remove it
+# convert to an array and use conditionals to see if any 9's are to be eaten
+require 'pry'
 
-"79712312" --> "7712312"
-"79797"    --> "777"
+class HungrySevens
+
+  def seven_ate9(str)
+    buffet = str.to_i.digits.reverse
+    buffet.each_with_index do |num, index|
+      case
+      when num == 9
+        if buffet[index -1] == 7 && buffet[index +1] == 7
+          buffet.delete_at(index)
+          #binding.pry
+        end
+      end
+    end
+    buffet.join("")
+  end
+end
