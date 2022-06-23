93ab29f265fe4b68fcc92cdd871aea2400550a9e
Joseph W Galvin <jwgalvin@gmail.com>
2022-06-22 20:06:28 -0600

    basic test names added.

--- lib/middle_character.rb
+++ lib/middle_character.rb
@@ -18,3 +18,8 @@
 # The middle character(s) of the word represented as a string.
 
 # take in a string and convert to an array. Find the middle character. If there is an even number find the middle to. return the middle characters.
+
+class Malcolm
+
+
+end
