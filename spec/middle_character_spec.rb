4ff3d54c49880a1db4502714d7669bd457c529af
Joseph W Galvin <jwgalvin@gmail.com>
2022-06-22 20:38:27 -0600

    added the rest of the tests

--- spec/middle_character_spec.rb
+++ spec/middle_character_spec.rb
@@ -12,7 +12,19 @@ RSpec.describe " checks challenges" do
   it "gets the middle_characters of an even one" do
     string = "test"
     malcom = Malcolm.new
-
     expect(malcom.get_middle(string)).to eq('es')
   end
+
+  it "gets the middle_characters of an even one" do
+    string = "middle"
+    malcom = Malcolm.new
+    expect(malcom.get_middle(string)).to eq('dd')
+  end
+
+  it "gets the middle_characters of an solo char" do
+    string = "a"
+    malcom = Malcolm.new
+    expect(malcom.get_middle(string)).to eq('a')
+  end
+
 end
