86be8e027ceca9018df9124ff2ef8251beca26ee
Joseph W Galvin <jwgalvin@gmail.com>
2022-06-24 15:22:51 -0600

    tests added

---
+++ spec/keep_hydrated_spec.rb
@@ -0,0 +1,23 @@
+require 'rspec'
+require 'pry'
+require './lib/keep_hydrated'
+
+RSpec.describe " checks challenges" do
+  it "see if I round the right way" do
+    time = 3
+    water_needed = Hydrate.new
+    expect(water_needed.litres(time)).to eq(1)
+  end
+
+  it "see if I round the right way" do
+    time = 6.7
+    water_needed = Hydrate.new
+    expect(water_needed.litres(time)).to eq(3)
+  end
+
+  it "see if I round the right way" do
+    time = 11.8
+    water_needed = Hydrate.new
+    expect(water_needed.litres(time)).to eq(5)
+  end
+end
