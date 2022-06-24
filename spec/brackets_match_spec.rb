ec5618f257a3060a9c2a5beb24560c42a415f95f
Joseph W Galvin <jwgalvin@gmail.com>
2022-06-23 18:08:37 -0600

    add some spaces between tests

--- spec/brackets_match_spec.rb
+++ spec/brackets_match_spec.rb
@@ -8,11 +8,13 @@ RSpec.describe " checks challenges" do
     check = PairedBrackets.new
     expect(check.matched_set?(string)).to eq(true)
   end
+
   it "see if I get the expected array" do
     string = '{()}'
     check = PairedBrackets.new
     expect(check.matched_set?(string)).to eq(true)
   end
+
   it "see if I get the expected array" do
     string = '({[]}{[]})'
     check = PairedBrackets.new
@@ -24,11 +26,13 @@ RSpec.describe " checks challenges" do
     check = PairedBrackets.new
     expect(check.matched_set?(string)).to eq(false)
   end
+
   it "see if I get the expected array" do
     string = '{[)][]}'
     check = PairedBrackets.new
     expect(check.matched_set?(string)).to eq(false)
   end
+  
   it "see if I get the expected array" do
     string = ']'
     check = PairedBrackets.new
