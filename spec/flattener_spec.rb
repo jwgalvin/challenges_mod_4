c160cf23c3e0e12a615f6c21d0b6f76223f61afe
Joseph W Galvin <jwgalvin@gmail.com>
2022-06-25 17:56:56 -0600

    add second test

--- spec/flattener_spec.rb
+++ spec/flattener_spec.rb
@@ -1,12 +1,17 @@
 require 'rspec'
 require 'pry'
-require './lib/bracket_matcher'
+require './lib/flattener'
 
 RSpec.describe " checks challenges" do
   it "see if I get the expected array" do
     nested_array = [1, 2, 3, [[4], 5], [[[6]]]]
-    flatten_me = Flattener.new
-    expect(nested_array.flattened(nested_array)).to eq([1, 2, 3, 4, 5, 6])
+    flatten_me = Flattner.new
+    expect(flatten_me.flattened(nested_array)).to eq([1, 2, 3, 4, 5, 6])
   end
 
+  it "see if I get the expected array" do
+    nested_array = [ 0, 1, 2, [ 3, 4 ] ]
+    flatten_me = Flattner.new
+    expect(flatten_me.flattened(nested_array)).to eq([ 0, 1, 2, 3, 4 ])
+  end
 end
