abe17e416599c8b7ecd1f7fdb4f700780a28da45
Joseph W Galvin <jwgalvin@gmail.com>
2022-06-25 17:38:53 -0600

    fix test method name

--- spec/flattener_spec.rb
+++ spec/flattener_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe " checks challenges" do
   it "see if I get the expected array" do
     nested_array = [1, 2, 3, [[4], 5], [[[6]]]]
     flatten_me = Flattener.new
-    expect(nested_array.matched_set?(string)).to eq([1, 2, 3, 4, 5, 6])
+    expect(nested_array.flattened(nested_array)).to eq([1, 2, 3, 4, 5, 6])
   end
-  
+
 end
