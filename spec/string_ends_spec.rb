b195c4c007d2b1098d1ef90b81b8edbe6f607486
Joseph W Galvin <jwgalvin@gmail.com>
2022-06-25 18:15:17 -0600

    update file path to completed

--- spec/string_ends_spec.rb
+++ spec/string_ends_spec.rb
@@ -1,7 +1,7 @@
 require 'rspec'
 require 'pry'
-require './lib/string_ends?'
-#require './lib/completed/string_ends?'
+
+require './lib/completed/string_ends?'
 
 RSpec.describe " checks challenges" do
   it "see if I get the expected string" do
