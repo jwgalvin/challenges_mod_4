675ba8ae60d8ab9c8566ad2ece381a2d5d15eb7e
Joseph W Galvin <jwgalvin@gmail.com>
2022-06-25 18:04:10 -0600

    create method file

---
+++ spec/string_ends_spec.rb
@@ -0,0 +1,20 @@
+require 'rspec'
+require 'pry'
+require './libflattener'
+#require './lib/completed/flattener'
+
+RSpec.describe " checks challenges" do
+  it "see if I get the expected string" do
+    string1 = 'abc'
+    string2 =  'bc'
+    compare = StringEnd.new
+    expect(compare.solution(string1, string2)).to eq(true)
+  end
+
+  it "see if I get the expected array" do
+    string1 = 'abc'
+    string2 =  'd'
+    compare = StringEnd.new
+    expect(compare.solution(string1, string2)).to eq(false)
+  end
+end
