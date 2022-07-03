96cae2158482e316cf957b7d0a78d22509b2f1ff
Joseph W Galvin <jwgalvin@gmail.com>
2022-07-03 09:14:22 -0600

    add spec file for sq digits

---
+++ spec/square_digits_spec.rb
@@ -0,0 +1,21 @@
+require 'rspec'
+require 'pry'
+require './lib/Array_diff'
+require './lib/completed/Array_diff'
+
+RSpec.describe " checks challenges" do
+  it "see if I get the expected array" do
+    a = [1,2]
+    b = [1]
+    check = Difference.new
+    expect(check.array_diff(a, b)).to eq([2])
+  end
+
+  it "see if I get the expected array" do
+    a = [1,2,2]
+    b = [2]
+    check = Difference.new
+    expect(check.array_diff(a, b)).to eq([1])
+  end
+  
+end
