5664ba016325946a5f0dd4cf5dada037ac004f30
Joseph W Galvin <jwgalvin@gmail.com>
2022-07-03 09:19:25 -0600

    fix class name

--- spec/square_digits_spec.rb
+++ spec/square_digits_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe " checks challenges" do
   it "see if I get the expected array" do
     int = 9119
    
-    to_sq = Square_each.new
+    to_sq = SquareEach.new
     expect(to_sq.square_digits(int)).to eq([2])
   end
 
