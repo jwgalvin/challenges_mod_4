b375db3eddf6034994185f7c8e100c7c2a7eefa2
Joseph W Galvin <jwgalvin@gmail.com>
2022-07-03 09:24:18 -0600

    fix test

--- spec/square_digits_spec.rb
+++ spec/square_digits_spec.rb
@@ -1,21 +1,14 @@
 require 'rspec'
 require 'pry'
-require './lib/Array_diff'
-require './lib/completed/Array_diff'
+require './lib/square_digits'
+#require './lib/completed/square_digits'
 
 RSpec.describe " checks challenges" do
   it "see if I get the expected array" do
     int = 9119
    
     to_sq = SquareEach.new
-    expect(to_sq.square_digits(int)).to eq([2])
+    expect(to_sq.square_digits(int)).to eq(811181)
   end
-
-#   it "see if I get the expected array" do
-#     a = [1,2,2]
-#     b = [2]
-#     check = Difference.new
-#     expect(check.array_diff(a, b)).to eq([1])
-#   end
   
 end
