185aaba0e0b49c301720ed12adae9a41b440ff9a
Joseph W Galvin <jwgalvin@gmail.com>
2022-07-03 11:21:48 -0600

    add second test

--- spec/find_missing_spec.rb
+++ spec/find_missing_spec.rb
@@ -11,4 +11,10 @@ RSpec.describe " checks challenges" do
     expect(absent_father.find_missing(array)).to eq(7)
   end
   
+  it "see if I get the expected array" do
+    array = [1, 3, 4, 5, 6, 7, 8, 9]
+   
+    absent_father = FindMissing.new
+    expect(absent_father.find_missing(array)).to eq(2)
+  end
 end
