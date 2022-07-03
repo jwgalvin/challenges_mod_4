d57888ee33346daab51ff8d787775e949748f7ef
Joseph W Galvin <jwgalvin@gmail.com>
2022-07-03 09:19:57 -0600

    add class and method

--- lib/square_digits.rb
+++ lib/square_digits.rb
@@ -3,3 +3,10 @@
 # For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
 
 # Note: The function accepts an integer and returns an integer
+
+Class SquareEach
+
+    def square_digits(int)
+
+    end
+end
