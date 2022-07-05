14bdcb9f7c08073098fbf77eda2b0f371522ee29
Joseph W Galvin <jwgalvin@gmail.com>
2022-07-05 13:22:11 -0600

    add test file for grasshopper summation

---
+++ spec/grasshopper_summaiton_spec.rb
@@ -0,0 +1,12 @@
+require 'rspec'
+require 'pry'
+#require './lib/find_missing'
+require './lib/completed/find_missing'
+
+RSpec.describe " checks challenges" do
+  it "see if I get the expected array" do
+    array = [1, 3, 5, 9, 11]
+   
+    absent_father = FindMissing.new
+    expect(absent_father.find_missing(array)).to eq(7)
+  end
