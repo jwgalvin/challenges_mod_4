43f7b28d3bbae73ed1720abcbfb20bc250fe4bd1
Joseph W Galvin <jwgalvin@gmail.com>
2022-07-05 13:45:55 -0600

    My solution and the 1 line solution found after the fact

--- lib/grass_summation.rb
+++ lib/grass_summation.rb
@@ -8,3 +8,16 @@
 
 # summation(8) -> 36
 # 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
+
+class Summation
+
+    def summation(num)
+        # sum = 0
+        # range = (0..num).to_a
+        # range.each do |int|
+        #     sum = sum + int
+        # end
+        # sum
+        (1..num).reduce(:+)
+    end
+end
