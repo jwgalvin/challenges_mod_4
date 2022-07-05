5ae748ff02eb80912cd8b7434decbdf7286d9c60
Joseph W Galvin <jwgalvin@gmail.com>
2022-07-05 13:46:26 -0600

    test updated for new class file location

--- spec/grasshopper_summaiton_spec.rb
+++ spec/grasshopper_summaiton_spec.rb
@@ -1,7 +1,7 @@
 require 'rspec'
 require 'pry'
-require './lib/grass_summation'
-#require './lib/completed/grass_summation'
+#require './lib/grass_summation'
+require './lib/completed/grass_summation'
 
 RSpec.describe " checks challenges" do
   it "see if I get the expected array" do
