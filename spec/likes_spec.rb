e774af4cd3d416d9f910a01dee1407f76f7036e0
Joseph W Galvin <jwgalvin@gmail.com>
2022-07-06 21:19:00 -0600

    add spec and test for likes

---
+++ spec/likes_spec.rb
@@ -0,0 +1,41 @@
+require 'rspec'
+require 'pry'
+#require './lib/grass_summation'
+require './lib/completed/grass_summation'
+
+RSpec.describe " checks challenges" do
+  it "see if I get the expected array" do
+    who = []
+   
+    likable = Liker.new
+    expect(likable.likes(who)).to eq("no one likes this")
+  end
+
+  it "see if I get the expected array" do
+    who = ["Peter"]
+   
+    likable = Liker.new
+    expect(likable.likes(who)).to eq("Peter likes this")
+  end
+
+  it "see if I get the expected array" do
+    who = ["Jacob", "Alex"]
+   
+    likable = Liker.new
+    expect(likable.likes(who)).to eq("Jacob and Alex like this")
+  end
+
+  it "see if I get the expected array" do
+    who = ["Max", "John", "Mark"]
+   
+    likable = Liker.new
+    expect(likable.likes(who)).to eq("Max, John and Mark like this")
+  end
+  
+  it "see if I get the expected array" do
+    who = ["Alex", "Jacob", "Mark", "Max"]
+   
+    likable = Liker.new
+    expect(likable.likes(who)).to eq("Alex, Jacob and 2 others like this")
+  end
+end
