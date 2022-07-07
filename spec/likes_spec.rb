a505af58476473e72d17d91a1356e04d94334450
Joseph W Galvin <jwgalvin@gmail.com>
2022-07-06 21:34:45 -0600

    updagte testing patjh

--- spec/likes_spec.rb
+++ spec/likes_spec.rb
@@ -1,7 +1,7 @@
 require 'rspec'
 require 'pry'
-require './lib/likes'
-#require './lib/completed/likes'
+#require './lib/likes'
+require './lib/completed/likes'
 
 RSpec.describe " checks challenges" do
   it "see if I get the expected array" do
