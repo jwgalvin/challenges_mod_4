31ead1fed157c6c8aa81a57eb942b5c91bdacc95
Joseph W Galvin <jwgalvin@gmail.com>
2022-07-06 22:10:06 -0600

    add test

--- spec/duplicate_encoder_spec.rb
+++ spec/duplicate_encoder_spec.rb
@@ -1,6 +1,6 @@
 require 'rspec'
 require 'pry'
-#require './lib/duplicate_encoder'
+require './lib/duplicate_encoder'
 #require './lib/completed/duplicate_encoder'
 
 RSpec.describe " checks challenges" do
@@ -9,6 +9,12 @@ RSpec.describe " checks challenges" do
    
   encode_me = DupeEncoder.new
     expect(encode_me.duplicate_encode(word)).to eq("(((")
+  end
+  it "see if I get the expected array" do
+    word = "recede"
+   
+  encode_me = DupeEncoder.new
+    expect(encode_me.duplicate_encode(word)).to eq("()()()")
   end
   
 end
