02661b33c05138f992562042bb04320d3ba36f1a
Joseph W Galvin <jwgalvin@gmail.com>
2022-07-07 21:26:53 -0600

    update tests

--- spec/detect_pangram_spec.rb
+++ spec/detect_pangram_spec.rb
@@ -5,13 +5,13 @@ require './lib/detect_pangram'
 
 RSpec.describe " checks challenges" do
   it "see if I get the expected array" do
-    member_list = "Nymphs blitz quick vex dwarf jog"
+    string = "Nymphs blitz quick vex dwarf jog"
     check = Pangram.new
     expect(check.pangram?(string)).to eq(true)
   end
 
   it "see if I get the expected array" do
-    member_list = "A quick brown fox jumps over the lazy dog"
+    string = "A quick brown fox jumps over the lazy dog"
     check = Pangram.new
     expect(check.pangram?(string)).to eq(true)
   end
