7c1951b957cf24a05fa549f6908df73e1600d8b9
Joseph W Galvin <jwgalvin@gmail.com>
2022-07-07 21:06:02 -0600

    add test with a capital letter

--- spec/detect_pangram_spec.rb
+++ spec/detect_pangram_spec.rb
@@ -7,6 +7,12 @@ RSpec.describe " checks challenges" do
   it "see if I get the expected array" do
     member_list = "Nymphs blitz quick vex dwarf jog"
     check = Pangram.new
-    expect(check.pangram?(string)).to eq(true])
+    expect(check.pangram?(string)).to eq(true)
+  end
+
+  it "see if I get the expected array" do
+    member_list = "A quick brown fox jumps over the lazy dog"
+    check = Pangram.new
+    expect(check.pangram?(string)).to eq(true)
   end
 end
