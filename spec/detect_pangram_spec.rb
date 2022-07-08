23df62e705ae6a17153a84fe1e1817ac08cd3278
Joseph W Galvin <jwgalvin@gmail.com>
2022-07-07 21:03:25 -0600

    add pangram test file

---
+++ spec/detect_pangram_spec.rb
@@ -0,0 +1,11 @@
+require 'rspec'
+require 'pry'
+#require './lib/completed/detect_pangram'
+require './lib/detect_pangram'
+
+RSpec.describe " checks challenges" do
+  it "see if I get the expected array" do
+    member_list = [[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]
+    list = Croquet.new
+    expect(list.open_or_senior(member_list)).to eq(["Open", "Open", "Senior", "Open", "Open", "Senior"])
+  end
