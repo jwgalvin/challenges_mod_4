7151c9cd901527a43bcf17dbecb93e47b6dfb762
Joseph W Galvin <jwgalvin@gmail.com>
2022-07-10 16:59:18 -0600

    my solution

--- lib/reverse_words.rb
+++ lib/reverse_words.rb
@@ -3,3 +3,14 @@
 # Examples
 # "This is an example!" ==> "sihT si na !elpmaxe"
 # "double  spaces"      ==> "elbuod  secaps"
+
+class Reverser
+    def reverse_words(str)
+        arr = str.scan(/\s+|\S+/)
+        reversed = []
+        arr.each do |word|
+            reversed << word.reverse
+        end
+        reversed.join
+    end
+end
