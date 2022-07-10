dfc4731bf662e124d69167f7744d4ae779c4d4c6
Joseph W Galvin <jwgalvin@gmail.com>
2022-07-10 16:37:52 -0600

    update test path

--- spec/ccent_to_year_spec.rb
+++ spec/ccent_to_year_spec.rb
@@ -1,7 +1,7 @@
 require 'rspec'
 require 'pry'
-# require './lib/completed/cent_to_year'
-require './lib/cent_to_year'
+require './lib/completed/cent_to_year'
+
 
 RSpec.describe " checks challenges" do
   it "see if I get the expected int" do
