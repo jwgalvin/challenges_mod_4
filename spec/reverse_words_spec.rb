c5498c22a77cabddf1c5eb24d4a4c800089652f7
Joseph W Galvin <jwgalvin@gmail.com>
2022-07-10 16:59:32 -0600

    tests for double spaces added.

--- spec/reverse_words_spec.rb
+++ spec/reverse_words_spec.rb
@@ -1,7 +1,7 @@
 require 'rspec'
 require 'pry'
-# require './lib/completed/cent_to_year'
-require './lib/cent_to_year'
+# require './lib/completed/reverse_words'
+require './lib/reverse_words'
 
 RSpec.describe " checks challenges" do
   it "see if I get the expected sentences" do
@@ -9,5 +9,11 @@ RSpec.describe " checks challenges" do
     check = Reverser.new
     expect(check.reverse_words(string)).to eq("sihT si na !elpmaxe")
   end
+  it "see if I get the expected sentences" do
+    string = "double  spaces"
+    check = Reverser.new
+    expect(check.reverse_words(string)).to eq("elbuod  secaps")
+  end
+
 
 end
