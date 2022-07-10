1be42bcf8e93420e7efd2e75bf01e034286fd0a0
Joseph W Galvin <jwgalvin@gmail.com>
2022-07-10 16:34:08 -0600

    my solution

--- lib/cent_to_year.rb
+++ lib/cent_to_year.rb
@@ -9,3 +9,30 @@
 # 1900 --> 19
 # 1601 --> 17
 # 2000 --> 20
+
+class CentYear
+    def century(year)
+        arr = year.digits.reverse
+        years = []
+        case 
+        when year < 100
+            return 1
+        when year < 1000
+            century = arr[0]
+            years << arr[1]
+            years << arr[2]
+            years = years.join.to_i
+        else
+            century = [arr[0]] 
+            century << arr[1]
+            century = century.join.to_i
+            years << arr[2]
+            years << arr[3]
+            years = years.join.to_i
+        end
+        if years >= 1
+            century +=1
+        end   
+        return century 
+    end
+end
